6b1e7a425b007810a7369f4990a98305d8cfdb40
Rebecca Govert <becci.govert@gmail.com>
2018-06-13 19:12:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(string)
    split_string = string.split(" ")
-   split_string.collect.to_a  { |word| word.reverse }
+   split_string.collect { |word| word.reverse }
+   split_string.to_a
 end 
 
