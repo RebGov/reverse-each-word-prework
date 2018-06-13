b47c2c0fe9aadb59885be1d5cd938763b07cb981
Rebecca Govert <becci.govert@gmail.com>
2018-06-13 16:50:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
 def reverse_each_word(string)
-  string_to_array_new = []
-  "".to_a = string_to_array_new
-  string_to_array_new.each do |word|
-    word.reverse
-  end
-  string_to_array_new
-end
-
+  my_array = "".split(/ /)
+  my_array.each do |word|
+    
