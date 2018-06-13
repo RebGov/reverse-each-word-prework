19b91e02edf8bb7d2192cb22d66674c8ca04ca3d
Rebecca Govert <becci.govert@gmail.com>
2018-06-13 19:25:42 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+def reverse_each_word(string)
+  split_string = string.split(" ")
+  reversed = split_string.collect { |word| word.reverse }
+  reversed.join(" ")
+end 
+
+
+=begin
+using .each method
+def reverse_each_word(string)
+  reversed = []
+  split_string = string.split(" ")
+  split_string.each do |word|
+    reversed << word.reverse 
+  end
+  reversed.join(" ")
+end
+=end 
